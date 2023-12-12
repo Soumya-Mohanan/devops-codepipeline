0a47946401bf1e576e12ad9948ae62227893084c
EC2 Default User <ec2-user@ip-172-31-8-214.eu-north-1.compute.internal>
2023-12-12 08:35:00 +0000

    change in html

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6r*(9rd=yrnxz&_(epcul*!m8rlzaa3d63ziudss0brp%bit=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['x23104767-car-rental-env.eba-ssxq8efg.eu-north-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
